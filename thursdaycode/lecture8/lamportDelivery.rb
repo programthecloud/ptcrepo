86480715264cd2b69d9c6c58044c46fd8161fae4
Joe Hellerstein <jmh@berkeley.edu>
2013-02-14 01:58:40 -0800

    lamport delivery

---
+++ thursdaycode/lecture8/lamportDelivery.rb
@@ -0,0 +1,57 @@
+require 'rubygems'
+require 'bud'
+require './ts_delivery'
+
+module LamportDelivery
+  include Bud
+  include TSDeliveryProtocol
+  import TSBestEffortDelivery => :bed
+  
+  
+  state do
+    lmax :cloq
+  end
+
+  bootstrap do
+    cloq <+ Bud::MaxLattice.new(0)
+  end
+    
+  bloom :plumbing do
+    bed.pipe_in <= pipe_in
+    pipe_out <= bed.pipe_out
+    pipe_sent <= bed.pipe_sent
+  end
+    
+  bloom :lamport do
+    # A process increments its counter before each event in that process;
+    cloq <+ pipe_in {|c| cloq + 1}
+    cloq <+ pipe_out {|c| cloq + 1}
+    
+    # On receiving a message, the receiver process sets its counter to be 
+    # greater than the maximum of its own value and the received value before 
+    # it considers the message received.
+    cloq <+ bed.pipe_out {|p| p.cloq}
+    stdio <~ pipe_out.inspected
+  end
+end
+
+class Doit
+  include Bud
+  include LamportDelivery
+end
+
+alice = Doit.new(:port => 12345)
+bob = Doit.new(:port => 23456)
+alice.run_bg
+bob.run_bg
+(1..3).each do |i|
+  alice.async_do do
+    alice.pipe_in <+ [["localhost:23456", "localhost:12345", 1, "Bob, you're a palindrome!", alice.cloq.current_value]]
+  end
+  sleep 1
+  bob.async_do do
+    bob.pipe_in <+ [["localhost:12345", "localhost:23456", 1, "You can't spell malice without Alice", bob.cloq.current_value]]
+  end
+  sleep 1
+end
+sleep 1
