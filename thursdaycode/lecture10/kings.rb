25c588725187d309645da8b0a17f7daeb550aea7
Peter Alvaro <palvaro@eecs.berkeley.edu>
2013-02-23 11:12:33 -0800

    fix

--- thursdaycode/lecture10/kings.rb
+++ thursdaycode/lecture10/kings.rb
@@ -1,7 +1,15 @@
 require 'rubygems'
 require 'bud'
 
+module KingProto
+  state do
+    interface input, :pretender, [:name]
+    interface output, :king, [:name]
+  end
+end 
+
 module KingChooser
+  include KingProto
   state do
     table :chamber, [:name] => [:arrival_time]
     scratch :heir, chamber.schema
@@ -11,7 +19,7 @@ module KingChooser
   bloom do
     chamber <= pretender{ |p| [p.name, budtime] }
     #heir <= chamber.group([], choose(:name))
-    heir <= chamber.argagg(:min, [:name], :arrival_time)
+    heir <= chamber.argagg(:min, [], :arrival_time)
     true_heir <= heir.group([], choose(:name))
     king <= true_heir{ |h| [h.name] }
     chamber <- (true_heir*chamber).rights(:name => :name)
@@ -29,6 +37,8 @@ a = KC.new(:trace => true, :port => 1234)
 a.pretender <+ [["Joe"]]
 a.tick
 a.pretender <+ [["Peter"],["Josh"]]
+a.tick
+a.pretender <+ [["Chris"]]
 a.tick
 a.tick
 
