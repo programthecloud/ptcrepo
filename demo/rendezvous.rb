ac5bfb2980a22a53c3f6c9140ecf13869f07554a
Joe Hellerstein <jmh@berkeley.edu>
2020-09-01 21:42:35 -0700

    remove broken versioning KVS

--- demo/rendezvous.rb
+++ demo/rendezvous.rb
@@ -12,7 +12,7 @@ module SpeakerPersist
     #scratch :spoken, [:subject, :val]
     table :spoken, [:subject, :val]
   end
-  bloom :persist do
+  bloom :speaker_persist do
     spoken <= speak
     #spoken <+ spoken
   end
@@ -27,55 +27,11 @@ module ListenerPersist
   state do
     table :listening, [:ident, :subject]
   end
-  bloom :persist do
+  bloom :listener_persist do
     listening <= listen
   end
   bloom do
     hear <= (speak*listening).pairs(:subject=>:subject) {|s,l| [l.ident, s.subject, s.val]}
-  end
-end
-
-module BothPersist
-  include RendezvousAPI
-  state do
-    table :spoken, [:subject, :val]
-    table :listening, [:ident, :subject]
-  end
-  bloom do
-    listening <= listen
-    spoken <= speak
-    hear <= (speak*listening).pairs(:subject=>:subject) {|s,l| 
-      [l.ident, s.subject, s.val]
-    }
-    hear <= (listen*spoken).pairs(:subject=>:subject) {|l,s| 
-      [l.ident, s.subject, s.val]
-    }
-  end
-end
-
-module VersionedSpeakerPersist
-  include RendezvousAPI
-  state do
-    scratch :spoken, [:subject] => [:val]
-    lmap :spoken_map
-    lmax :ticker
-  end
-
-  bootstrap do
-    ticker <+ Bud::MaxLattice.new(0)
-  end
-
-  bloom :persist do
-    ticker <+ (ticker + 1)
-    spoken_map <= speak {|s|
-      {s.subject => Bud::VersionLattice.new(s.val, Bud::MaxLattice.new(ticker.reveal))}
-    }
-    spoken <+ spoken_map.to_collection do |k, v|
-                  [k, v]
-    end
-  end
-  bloom do
-    hear <= (spoken*listen).pairs(:subject=>:subject) {|s,l| [l.ident, s.subject, s.val]}
   end
 end
 
--- demo/runner.rb
+++ demo/runner.rb
@@ -1,15 +1,18 @@
 require 'rubygems'
+require 'colorize'
 require 'bud'
 require './rendezvous_api'
 require './rendezvous'
 
 module Debug
   bloom do
-    stdio <~ hear.inspected
+    stdio <~ hear {|h| ["hear: #{h}".white]}
+    stdio <~ listen {|l| ["listen: #{l}".blue]}
+    stdio <~ speak {|s| ["speak: #{s}".red]}
   end
 end
 
-class Synchronous
+class Sync
   include Bud
   include SynchronousRendezvous
   include Debug
@@ -29,31 +32,34 @@ end
 
 class Both
   include Bud
-  include BothPersist
+  include SpeakerPersist
+  include ListenerPersist
   include Debug
 end
 
-class Mutable
+class MSPBoth
   include Bud
   include MutableSpeakerPersist
+  include ListenerPersist
   include Debug
-end
+end  
 
-class Versioned
-  include Bud
-  include VersionedSpeakerPersist
-  include Debug
-end
 
-l = Synchronous.new
-l.speak <+ [["news", "this is the first!"]]
+l = Sync.new
+l.run_bg()
 
-l.listen <+ [["peter", "news"]]
-l.tick
+puts "tick #{l.budtime}: speak and wei listens"
+l.sync_do{ 
+  l.speak <+ [["#mountain", "1st msg sent at time #{l.budtime}"]]
+  l.listen <+ [["wei", "#mountain"]]
+}
 
-# # uncomment me for mutable or versioned KVS
-# l.speak <+ [["news", "and this is the second."]]
-# l.tick
+puts "tick #{l.budtime}: ashima listens"
+l.sync_do{
+  l.listen <+ [["ashima", "#mountain"]]
+}
 
-# l.listen <+ [["paul", "news"]]
-# l.tick
+puts "tick #{l.budtime}: speak"
+l.sync_do{
+  l.speak <+ [["#mountain", "next msg sent at time #{l.budtime+1}"]]
+}
